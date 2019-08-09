8d0fa66c0d9b18a883420524ff6417c64d9d7937
Sergey Kasatikov <skasatikov@team.amocrm.com>
2019-08-09 17:02:34 +0300

    change the name of src dir, some lil fixes to response logging

--- gulpfile.js
+++ gulpfile.js
@@ -4,42 +4,39 @@ var gulp = require('gulp'),
     FormData = require('form-data'),
     fs = require('fs'),
     http = require('http'),
-    // decompress = require('gulp-decompress'),
-    // log = require('fancy-log'),
-    // combiner = require('stream-combiner2'),
-    // filesExist = require('files-exist'),
-    // sizeOf = require('image-size'),
-    // color = require('gulp-color'),
-    // scan = require('gulp-scan'),
-    // splitLines = require('split-lines'),
-    // loadJsonFile = require('load-json-file'),
-    // _ = require('underscore'),
-    // path = require('path'),
-    // clean = require('gulp-clean'),
     config = require('./config.js');
 
 
   gulp.task('bumpWidgetVersion', function() {
-    return gulp.src('src/manifest.json', { base: './'})
+    return gulp.src('widget/manifest.json', { base: './'})
      .pipe(bump())
      .pipe(gulp.dest('./'));
   });
 
   gulp.task('zipWidget', function() {
-    return gulp.src('src/**')
+    return gulp.src('widget/**')
       .pipe(zip('widget.zip'))
       .pipe(gulp.dest('./'));
   });
 
   gulp.task('uploadWidget', function(cb) {
-    var widget_form = new FormData();
+    var widget_form = new FormData(),
+        body = '';
     widget_form.append('secret', config.WIDGET.secret_key);
     widget_form.append('widget', config.WIDGET.widget_code);
     widget_form.append('amouser',  config.ACCOUNT.amouser);
     widget_form.append('amohash', config.ACCOUNT.amohash);
     widget_form.append('widget', fs.createReadStream('widget.zip'));
-    widget_form.submit('https://widgets.amocrm.ru/' + config.ACCOUNT.subdomain + '/upload', function(err, res) {
-      console.log(res);
+    widget_form.submit('https://widgets.amocrm.ru/' + config.ACCOUNT.subdomain + '/upload', function(err, response) {
+      if (err) return err;
+      response.on('data', function (chunk) {
+        body += chunk;
+      });
+      response.on('end', function () {
+        if (body.indexOf('{"response":true}')) {
+          console.log('Widget successfully uploaded');
+        }
+      });
       cb();
     });
   });
